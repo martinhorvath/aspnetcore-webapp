24cbdd6666535f2cc64e0c8ff14666a7a8cf89b4
Martin Horvath <martin.horvath@primebird.com>
2021-01-14 20:09:41 +0100

    Added code for reading from db

---
+++ Entities/Airport.cs
@@ -0,0 +1,13 @@
+namespace aspnetcore_webapp.Entities
+{
+  public class Airport
+  {
+    public string iata_code;
+    public string airport;
+    public string city;
+    public string state;
+    public string country;
+    public double latitude;
+    public double longitude;
+  }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using aspnetcore_webapp.Entities;
+using Npgsql;
 
 namespace aspnetcore_webapp.Pages
 {
     public class IndexModel : PageModel
     {
+        public IList<Airport> airports;
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,7 +22,38 @@ namespace aspnetcore_webapp.Pages
 
         public void OnGet()
         {
+            this.airports = GetAirports();
+        }
 
+        private IList<Airport> GetAirports()
+        {
+            IList<Airport> airports = new List<Airport>();
+            var connString = "Host=localhost;Username=postgres;Password=newton243;Database=postgres";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                using (var cmd = new NpgsqlCommand("SELECT * FROM flightnet_airports", conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Airport myAirport = new Airport();
+                            myAirport.airport = (string)reader["airport"];
+                            myAirport.city = (string)reader["city"];
+                            myAirport.country = (string)reader["country"];
+                            myAirport.iata_code = (string)reader["iata_code"];
+                            myAirport.latitude = 0;
+                            myAirport.longitude = 0;
+                            myAirport.state = (string)reader["state"];
+                            airports.Add(myAirport);
+                        }
+                    }
+                }
+            }
+            return airports;
         }
     }
 }
